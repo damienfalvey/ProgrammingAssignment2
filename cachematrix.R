c7ddb0b235c3acb265b23ae1b7f32e829e571b6d
Damien Falvey <damienfalvey@gmail.com>
2015-01-16 14:42:21 +0000

    Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix is a function that returns a list of functions. Its puspose is to store a martix and a cached value of the inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
 
+	get<-function() x
+   ## Declare Getters & Setters for Matrix
+	setmatrix<-function(solve) m<<- solve
+	getmatrix<-function() m
+   ## Assign Getters & Setters in List
+	list(set=set, get=get,
+		setmatrix=setmatrix, ## Assigns the stored matrix
+		getmatrix=getmatrix) ## returns the stored matrix
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of a "special" matrix created with makeCacheMatrix
+cacheSolve <- function(x=matrix(), ...) {
+   ## get the cached value
+	m<-x$getmatrix()
+   ## If a cached value exists return it
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## otherwise get the matrix, caclulate the inverse and store it in the cache
+	matrix<-x$get()
+	m<-solve(matrix, ...)
+	x$setmatrix(m)
+	m
 }
+
+## --- Test ---
+
+a<-makeCacheMatrix()
+a$set(matrix(1:4,2,2))
+cacheSolve(a)
